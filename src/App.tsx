7c82f9bcf6b22a3891247f00febcc898f4a643ef
HackerManOSU <130389151+HackerManOSU@users.noreply.github.com>
2024-10-31 20:08:53 +0000

    ❌ import 'React'

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import OddsPage from './components/OddsPage';
